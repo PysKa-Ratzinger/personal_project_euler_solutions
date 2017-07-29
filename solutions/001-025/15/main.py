91229fb7ab90e245cecabcb426f61c6efcf9ef0b
PysKa Ratzinger <ricardojorge512@hotmail.com>
2017-07-29 16:04:58 +0100

    Problem 15 solved

---
+++ solutions/001-025/15/main.py
@@ -0,0 +1,18 @@
+def factorial(n):
+    if(n == 0):
+        return 0
+    if(n == 1):
+        return 1
+    res = 1
+    for i in range(2, n+1):
+        res *= i
+    return res
+
+def combination(n, k):
+    return factorial(n) // (factorial(k) * factorial(n - k))
+
+def main():
+    print(combination(40, 20))
+
+main()
+
